5320af0aad4a146345898a13579e2ad0604d54bc
Israel Batista <israel.silvino@gmail.com>
2023-10-19 19:16:10 -0300

    PowerLevel10k instalado

--- .zsh-init/10-prompt.zsh
+++ .zsh-init/10-prompt.zsh
@@ -3,12 +3,21 @@
 # Criar pasta .zsh
 mkdir -p $HOME/.zsh
 
-# Baixar o spaceship
-if [ ! -d $HOME/.zsh/spaceship ]; then
-	git clone --depth=1 https://github.com/spaceship-prompt/spaceship-prompt.git "$HOME/.zsh/spaceship"
-fi
+# === Spaceship ===
 
-# Inicializar o Spaceship
+# # Baixar o spaceship
+# if [ ! -d $HOME/.zsh/spaceship ]; then
+# 	git clone --depth=1 https://github.com/spaceship-prompt/spaceship-prompt.git "$HOME/.zsh/spaceship"
+# fi
+#
+# # Inicializar o Spaceship
+#
+# source "$HOME/.zsh/spaceship/spaceship.zsh"
 
-source "$HOME/.zsh/spaceship/spaceship.zsh"
+# === Powerlevel10k ===
+# Instalar powerlevel10k
+#
+source ~/.config/powerlevel10k/powerlevel10k.zsh-theme
 
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
