43291ee529870ff785231c7705bec1e7197042ab
Israel Batista <israel.silvino@gmail.com>
2024-12-27 07:09:28 -0300

    feat: Simplify neovim config (no overbloated vscode-like bs)

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,14 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.g.trouble_lualine = true
+
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.signcolumn = "number"
+vim.opt.cursorline = true
+
