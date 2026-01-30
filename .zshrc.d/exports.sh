225b05469aa6a5183af4d11d6303cfad8e35e4ec
Israel Batista <israel.silvino@gmail.com>
2026-01-30 16:41:53 -0300

    Add odin to exports

--- .zshrc.d/exports.sh
+++ .zshrc.d/exports.sh
@@ -5,6 +5,9 @@ export PATH="$PATH:/opt/nvim-linux64/bin"
 export PATH="$PATH:$HOME/.local/opt/"
 export PATH="$PATH:$HOME/.local/bin/"
 
+# Local odin
+export PATH="$PATH:$HOME/.local/opt/odin/"
+
 # Libraries
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.local/lib/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.local/lib64/
